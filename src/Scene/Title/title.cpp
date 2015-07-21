437f9e74b7f309483c91b2aadc7681050538a34d
Lacty <yanai@Yanai-no-Air.lan>
2015-07-21 19:33:12 +0900

    日本語描画[動作確認済み]

--- src/Scene/Title/title.cpp
+++ src/Scene/Title/title.cpp
@@ -1,19 +1,20 @@
 
 #include "title.h"
 #include "cinder/gl/gl.h"
-#include "../../Utility/utility.h"
 
 
 using namespace ci;
 
 Title::Title() {
-  font = Font(ci::app::loadAsset("rounded-l-mplus-1c-regular.ttf"), 50);
+  font = Font(ci::app::loadAsset("rounded-l-mplus-1c-bold.ttf"), 50);
 }
 
 void Title::update() {}
 
 void Title::draw() {
-    //gl::drawCube(Vec3f::zero(),
-    //             Vec3f(50, 50, 50));
-    ly::drawStringCenterdUtf8("あばば", Vec2f::zero(), ColorA(1, 1, 1), font);
+  using namespace ci::gl;
+  drawStringCentered("私は梁井nice to meet you",
+                     Vec2f::zero(),
+                     ColorA(1, 1, 1),
+                     font);
 }
